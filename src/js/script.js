42cb49acda36600ce2fc00c5a5b75d0893fe0dca
alwaysdri <c16716008@gmail.com>
2024-09-12 11:06:56 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('mousemove', function(event) {
+    const menu = document.getElementById('menu');
+    const mouseX = event.clientX;
+    
+    if (mouseX < 50) {
+        menu.classList.add('show');//adiciona a classe show
+
+    } else {
+        if (!menu.matches(':hover') && mouseX > 250) {
+            menu.classList.remove('show');
+        }//remove a classe show 
+    }
+});
+
+document.getElementById('menu').addEventListener('mouseover', function() {
+    this.classList.add('show');
+});//menu aberto com o mouse em cima
+
+document.getElementById('menu').addEventListener('mouseout', function(event) {
+    if (!event.relatedTarget || !this.contains(event.relatedTarget)) {
+        this.classList.remove('show');
+    }
+});//fecha menu quando mouse sai
+
